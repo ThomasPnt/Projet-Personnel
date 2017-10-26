3276b53c5818a4d8ff6afedac1b431d996a4dce5
Unknown <thomaspoint@gmail.com>
2017-10-26 16:16:47 +0200

    mobile end

--- OnePageTest/app.js
+++ OnePageTest/app.js
@@ -1,5 +1,12 @@
-// var twitter = document.querySelector('#twitter');
-//
-// twitter.addEventListener('mouseover',function(){
-// twitter.style.color = "#3cf";
-// });
+var icon = document.querySelectorAll('i');
+
+for (var i = 0; i < icon.length; i++) {
+  icon[i].addEventListener('mouseover', function(event){
+   var target = icon[i];
+    event.target.style.backgroundColor = 'black';
+
+   setTimeout(function() {
+    event.target.style.backgroundColor = "";
+  },100);
+  },false);
+};
