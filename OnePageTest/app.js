94c6c7a4a0324c996b48cbeeccffe2948cf3c883
Unknown <thomaspoint@gmail.com>
2017-10-27 13:16:39 +0200

    js

--- OnePageTest/app.js
+++ OnePageTest/app.js
@@ -10,3 +10,33 @@ for (var i = 0; i < icon.length; i++) {
   },100);
   },false);
 };
+
+var program = document.querySelector('.program');
+var ligne = document.querySelectorAll('.line');
+var newp = document.createElement('p');
+var h3 = document.getElementsByName('h3');
+var descriptif = document.getElementById('texte');
+
+// function text(){
+//   program.appendChild(newp);
+// }
+//
+// function efface(){
+//   newp.innerHTML = "";
+// }
+
+for (var i = 0; i < ligne.length; i++) {
+  ligne[i].addEventListener('mouseover' , function(event){
+    this.style.backgroundColor = '#E0A5A0';
+    descriptif.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti itaque iure saepe sit, dolorem quia! At expedita eius molestiae odit quia animi vero ad corrupti?</p>';
+    // text();
+    for (var ii = 0; ii < ligne.length; ii++){
+      ligne[ii].addEventListener('mouseout',function(event){
+        this.style.backgroundColor = "";
+        descriptif.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni sapiente natus pariatur fuga, ipsam odio inventore fugit at aperiam dolorum! Sit numquam error culpa laudantium aliquid, eaque dicta repudiandae voluptates?50</p>';
+        event.preventDefault();
+        // efface();
+        });
+      }
+    });
+  };
