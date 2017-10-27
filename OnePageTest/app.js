c4292733535de3fc7587ba9d5199827f3c61c494
Unknown <thomaspoint@gmail.com>
2017-10-27 15:53:00 +0200

    Finaaal

--- OnePageTest/app.js
+++ OnePageTest/app.js
@@ -3,12 +3,13 @@ var icon = document.querySelectorAll('i');
 for (var i = 0; i < icon.length; i++) {
   icon[i].addEventListener('mouseover', function(event){
    var target = icon[i];
-    event.target.style.backgroundColor = 'black';
-
-   setTimeout(function() {
-    event.target.style.backgroundColor = "";
-  },100);
-  },false);
+    event.target.style.backgroundColor = 'white';
+    for (var i = 0; i < icon.length; i++) {
+      this.addEventListener('mouseout', function(event){
+      event.target.style.backgroundColor = "";
+      });
+    }
+  });
 };
 
 var program = document.querySelector('.program');
